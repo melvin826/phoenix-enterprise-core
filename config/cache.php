27bb9718c539611152fcab29f96608ee5a20290a
Melvin M Bertulfo <melvinbertulfo@gmail.com>
2026-09-13 22:46:06 +0800

    PHX-M1: add in-memory cache store for tests

--- config/cache.php
+++ config/cache.php
@@ -3,6 +3,10 @@
 return [
     'default' => env('CACHE_STORE', 'redis'),
     'stores' => [
+        'array' => [
+            'driver' => 'array',
+            'serialize' => false,
+        ],
         'file' => [
             'driver' => 'file',
             'path' => storage_path('framework/cache/data'),
