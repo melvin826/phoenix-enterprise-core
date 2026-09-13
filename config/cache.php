da219c218628d4cfd41029f69bc902602fb6c02b
Melvin M Bertulfo <melvinbertulfo@gmail.com>
2026-09-13 22:41:35 +0800

    PHX-M1: add isolated cache configuration

---
+++ config/cache.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'default' => env('CACHE_STORE', 'redis'),
+    'stores' => [
+        'file' => [
+            'driver' => 'file',
+            'path' => storage_path('framework/cache/data'),
+            'lock_path' => storage_path('framework/cache/data'),
+        ],
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => env('CACHE_REDIS_CONNECTION', 'cache'),
+            'lock_connection' => env('CACHE_REDIS_LOCK_CONNECTION', 'default'),
+        ],
+    ],
+    'prefix' => env('CACHE_PREFIX', 'phoenix_dev_cache_'),
+];
